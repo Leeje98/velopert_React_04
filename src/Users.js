27cab121dfee2d33e34d30039d282d1dda22adb6
lee Jeong Eun <wjd7dms7@naver.com>
2022-12-28 15:53:32 +0900

    useReducer 사용하여 코드 수정

--- src/Users.js
+++ src/Users.js
@@ -1,32 +1,61 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useReducer, useState } from "react";
 import axios from "axios";
 
+
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'LOADING':
+      return {
+        loading: true,
+        data: null,
+        error: null
+      };
+    case 'SUCCESS':
+      return {
+        loading: false,
+        data: action.data,
+        error: null
+      };
+    case 'ERROR':
+      return {
+        loading: false,
+        data: null,
+        error: action.error
+      };
+    default:
+      throw new Error(`Unhandled action type: ${action.type}`);
+  }
+}
+
+
+
 function Users() {
-  const [users, setUsers] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null); 
+  const [state, dispatch] = useReducer(reducer, {
+    loading: false,
+    data: null,
+    error: null
+  })
+ 
 
   const fetchUsers = async () => {
+    dispatch({ type: 'LOADING' })
     try {
-      setError(null);
-      setUsers(null);
-      // 요청이 시작 할 때에는 error 와 users 를 초기화하고
-      setLoading(true);
-      // loading 상태를 true 로 바꿉니다.
-      const response = await axios.get(   // get: 데이터 조회
+      const response = await axios.get(
         'https://jsonplaceholder.typicode.com/users'
       );
-      setUsers(response.data); // 데이터는 response.data 안에 들어있습니다.
+      dispatch({ type: 'SUCCESS', data: response.data });
     } catch (e) {
-      setError(e);
+      dispatch({ type: 'ERROR', error: e });
     }
-    setLoading(false);
   };
 
 
   useEffect(() => {
     fetchUsers();
   }, []);
+
+  const { loading, data: users, error } = state;
 
   if (loading) return <div>로딩중...</div>
   if (error) return <div>에러가 발생했습니다</div>
