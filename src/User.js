e7b1f0ebbda2e644b6efbe5349557c806a0eaecb
lee Jeong Eun <wjd7dms7@naver.com>
2022-12-29 14:21:01 +0900

    Context 와 함께 사용하기

--- src/User.js
+++ src/User.js
@@ -1,27 +1,18 @@
-import React from "react";
-import axios from "axios";
-import { useAsync } from "react-async"; // 라이브러리 사용시 로딩중... 만 뜨는 상황발생 react 18 버전 오차인듯함 index.js 파일의 <React.StrictMode>태그 주석처리하면 해결됨
- 
-
-
-async function getUser({ id }) {    // useAsync 를 사용할 때에는 프로미스를 반환하는 함수의 파라미터를 객체형태로 해주어야 한다
-  const response = await axios.get(
-    `https://jsonplaceholder.typicode.com/users/${id}`
-  );
-  return response.data;
-}
+import React, { useEffect } from 'react';
+import { useUsersState, useUsersDispatch, getUser } from './UsersContext';
 
 function User({ id }) {
-  const { data: user, error, isLoading } = useAsync({
-    promiseFn: getUser,
-    id,
-    watch: id
-  });
+  const state = useUsersState();
+  const dispatch = useUsersDispatch();
+  useEffect(() => {
+    getUser(dispatch, id);
+  }, [dispatch, id]);
 
-  if (isLoading) return <div>로딩중..</div>;
+  const { data: user, loading, error } = state.user;
+
+  if (loading) return <div>로딩중..</div>;
   if (error) return <div>에러가 발생했습니다</div>;
   if (!user) return null;
-
   return (
     <div>
       <h2>{user.username}</h2>
--- src/Users.js
+++ src/Users.js
@@ -1,31 +1,21 @@
 import React, { useState } from 'react';
-import axios from 'axios';
-import { useAsync } from 'react-async';
+import { useUsersState, useUsersDispatch, getUsers } from './UsersContext';
 import User from './User';
-
-
-// useAsync 에서는 Promise 의 결과를 바로 data 에 담기 때문에,
-// 요청을 한 이후 response 에서 data 추출하여 반환하는 함수를 따로 만듬
-async function getUsers() {
-  const response = await axios.get(
-    'https://jsonplaceholder.typicode.com/users'
-  );
-  return response.data;
-}
 
 function Users() {
   const [userId, setUserId] = useState(null);
+  const state = useUsersState();
+  const dispatch = useUsersDispatch();
 
-  // const [state, refetch] = useAsync(getUsers, [], true);  // 1. 배열 대신
-  // const { data: users, error, isLoading, reload } = useAsync({  // 2. useAsync 를 사용할 때에는 프로미스를 반환하는 함수의 파라미터를 객체형태로  // 1. 렌더링 시 데이터 바로 호출
-  const { data: users, error, isLoading, run } = useAsync({  // 2. 버튼을 눌러야 데이터 호출
-    // promiseFn: getUsers
-    deferFn: getUsers
-  });
+  const { data: users, loading, error } = state.users;
+  const fetchData = () => {
+    getUsers(dispatch);
+  };
 
-  if (isLoading) return <div>로딩중..</div>;
+  if (loading) return <div>로딩중..</div>;
   if (error) return <div>에러가 발생했습니다</div>;
-  if (!users) return <button onClick={run}>불러오기</button>;
+  if (!users) return <button onClick={fetchData}>불러오기</button>;
+
   return (
     <>
       <ul>
@@ -39,15 +29,10 @@ function Users() {
           </li>
         ))}
       </ul>
-      {/* <button onClick={reload}>다시 불러오기</button>   // 1. 렌더링 시 데이터 바로 호출 */} 
-      <button onClick={run}>다시 불러오기</button>  {/*  // 2. 버튼을 눌러야 데이터 호출 */}
+      <button onClick={fetchData}>다시 불러오기</button>
       {userId && <User id={userId} />}
     </>
   );
 }
 
 export default Users;
-
-
-
-// * 렌더링하는 시점이 아닌 사용자의 특정 인터랙션에 따라 API 를 호출하고 싶을 땐 promiseFn 대신 deferFn 을 사용하고, reload 대신 run 함수를 사용하면 된다
